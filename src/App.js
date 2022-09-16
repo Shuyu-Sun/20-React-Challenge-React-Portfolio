7655a041fcb1dd6ddd006ce981d39561cb5d83d2
Stella Sun <sunshuyu0413@163.com>
2022-09-16 13:35:54 -0400

    basic app.js file

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
+};
+export default App;
