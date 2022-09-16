963ac664256c179845e1b030cf7a7d6bad54360b
Stella Sun <sunshuyu0413@163.com>
2022-09-16 16:50:18 -0400

    portfolio and resume

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,24 +1,24 @@
 import React from "react";
-
 function Navigation(props) {
-  const tabs = ["About", "Portfolio", "Contact", "Resume"];
-  return (
-    <ul className="flex-row mobile-view">
-      {tabs.map((tab) => (
-        <h4 className="nav-item" key={tab}>
-          <a
-            href={"#" + tab.toLowerCase()}
-            onClick={() => props.handlePageChange(tab)}
-            className={
-              props.currentPage === tab ? "nav-link active" : "nav-link" 
-            }
-          >
-            {tab}
-          </a>
-        </h4>
-      ))}
-    </ul>
-  );
+	const { currentTab, setCurrentTab } = props;
+return (
+<nav>
+			<ul className="flex-row mobile-view">
+				<li className={currentTab === "about" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("about")}>About Me</span>
+				</li>
+				<li className={currentTab === "portfolio" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("portfolio")}>Portfolio</span>
+				</li>
+				<li className={currentTab === "contact" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("contact")}>Contact</span>
+				</li>
+				<li className={currentTab === "resume" ? "mx-2 navActive" : "mx-2"}>
+					<span onClick={() => setCurrentTab("resume")}>Resume</span>
+				</li>
+			</ul>
+		</nav>
+	);
 }
 
 export default Navigation;
