4d1b0798615186244d5bac95e5077971cc2400db
Stella Sun <sunshuyu0413@163.com>
2022-09-16 15:00:02 -0400

    added components folder & updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from "react";
-import Header from "./components/Header";
+import React, { useState } from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
 import Footer from "./components/Footer";
+import Header from "./components/Header";
+// import Navigation from "./components/Navigation";
+import Portfolio from "./components/Portfolio";
+import Resume from "./components/Resume";
 import "./App.css";
 
-const App = () => {
+function App() {
+  const [currentTab, setCurrentTab] = useState("about");
+
+  const renderTab = () => {
+    if (currentTab === "About") {
+      return <About />;
+    }
+    if (currentTab === "Contact") {
+      return <Contact />;
+    }
+    if (currentTab === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentTab === "Resume") {
+      return <Resume />;
+    }
+  };
+
   return (
-    <>
+    <div>
       <div>
-        <Header />
+        <Header currentTab={currentTab} setCurrentTab={setCurrentTab}></Header>
       </div>
       <div>
-        <Footer />
+        <main>{renderTab()}</main>
       </div>
-    </>
+      <div>
+        <Footer></Footer>
+      </div>
+    </div>
   );
-};
+}
+
 export default App;
